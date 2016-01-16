29b942165e5a42fa3ffbfbf316a7db480c97364b
Roger Day <c.roger.day@gmail.com>
2016-01-16 13:46:17 +0000

    start with changes to index

--- lib/rkive/clients/cl/indexer.py
+++ lib/rkive/clients/cl/indexer.py
@@ -1,34 +1,23 @@
-from rkive.clients.cl.opts import GetOpts, BaseAction
 import rkive.clients.log
+import rkive.clients.cl.opts
 import os.path
 import argparse
 from logging import getLogger
-import rkive.clients.files
+import sys
+from rkive.index.musicfile import MusicFile
+from rkive.index.index import Index
 
-class Indexer(Index):
+class IndexClient(Index):
 
-    def run(self):
+    def run(self, log=None):
         try:
-            self.recursive = False
-            go = GetOpts(parent=self)
-            go.p.add_argument('--base', type=str, help="folder in which look for files", action=BaseAction)
+            go = rkive.clients.cl.opts.GetOpts(parent=self)
+            go.p.add_argument('--base', required=True, nargs=1, help="full path to base of files", action=rkive.clients.cl.opts.BaseAction)
+            go.p.add_argument('--dry-run', required=False, help="Do not add to database")
+            go.p.add_argument('--debug', required=False, help="lots of logs")
             go.get_opts()
-            self.console = True
-            logs = 'logs/indexer.log'
-            clients.log.LogInit().set_logging(location=log,filename='index.log', debug=self.debug, console=self.console)
-            log = getLogger('Rkive.Indexer')
-            if (not self.base):
-                self.base='.'
-            self.visit_files(self.add_to_index) 
+            rkive.clients.log.LogInit().set_logging(location=log,filename='index.log', debug=self.debug, console=self.console)
         except SystemExit:
             pass
-        except TypeNotSupported as e:
-            log = getLogger('Rkive.Tagger')
-            log.fatal("Type not supported")
-        except FileNotFound as e:
-            log = getLogger('Rkive.Tagger')
-            log.fatal("Type not supported")
 
-    def add_to_index(self, path):
-        pass
-
+          
--- lib/rkive/clients/files.py
+++ lib/rkive/clients/files.py
@@ -1,14 +1,16 @@
 import os
 
 class Files(object):
-    def visit(self, base='.', funcs=[], exclude=[]):
+    def visit(self, base='.', funcs=[], exclude=None):
         for root, dirs, files in os.walk(base, topdown=False):
             for name in files:
                 print(os.path.join(root, name))
+                if (exclude and exclude(root, name)):
+                    continue
                 fp = os.path.join(root, name)
                 for func in funcs:
                     func(fp)
             for name in dirs:
                 print(os.path.join(root, name))
                 base = os.path.join(root, name)
-                self.visit(base, funcs=funcs)
+                self.visit(base, funcs=funcs, exclude=exclude)
