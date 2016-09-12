6969d332e6000cbe1e56663c2b50ddf32e3f872f
roger.day <c.roger.day@gmail.com>
2016-09-12 13:16:32 +0100

    add tags file

--- src/rkive/index/mediaindex.py
+++ src/rkive/index/mediaindex.py
@@ -5,12 +5,12 @@ from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from rkive.index.musicfile import MusicFile
 
 Base = declarative_base()
-     
+
 class MusicTrack(Base, MusicFile):
     __tablename__ = 'MusicTrack'
     id_track = Column('idMusicTrack', Integer, primary_key=True)
-    Column('discnumber', String)   
-    Column('disctotal', String)   
+    Column('discnumber', String)
+    Column('disctotal', String)
     Column('album', String)
     Column('title', String)
     Column('filepath', String)
@@ -24,11 +24,11 @@ class MusicTrack(Base, MusicFile):
 
     def __init__(self, path):
         MusicFile.__init__(path)
-        
+
 # Create an engine that stores data in the local directory's
 # sqlalchemy_example.db file.
 engine = create_engine('postgresql://postgres:postgres@192.168.1.155/MediaIndex')
- 
+
 # Create all tables in the engine. This is equivalent to "Create Table"
 # statements in raw SQL.
 Base.metadata.create_all(engine)
