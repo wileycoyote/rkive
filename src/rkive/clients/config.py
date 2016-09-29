b4fd9aff0552661d17ead62f8d4ef71186b7164d
roger.day <c.roger.day@gmail.com>
2016-09-29 20:55:47 +0100

    add tests for config

--- src/rkive/clients/config.py
+++ src/rkive/clients/config.py
@@ -9,17 +9,33 @@ class Config:
         self.connections = []
         self.home = home
 
-    def read(self):
+    def read_sources(self):
         #read the sources
         source = os.path.join(self.home, 'sources.yml')
-        with open(source) as s:
-            d = load(s)
-            for k, v in d.items():
-                reverse_key = v.lowercase()
-                if not reverse_key in self.sources:
-                    self.sources[reverse_key] = []
-                self.sources[reverse_key].append(k)
+        try:
+            with open(source) as s:
+                d = load(s)
+                for k, v in d.items():
+                    reverse_key = v.lower()
+                    if not reverse_key in self.sources:
+                        self.sources[reverse_key] = []
+                    self.sources[reverse_key].append(k)
+        except EnvironmentError:
+            return False
+        if self.sources is None:
+            return False
+        return True
+
+    def read_connections(self):
         conns_cfg = os.path.join(self.home, 'connections','default.yml')
+        try:
+            with open(conns_cfg) as cf:
+                pass
+        except EnvironmentError:
+            return False
+        if self.connections is None:
+            return False
+        return True
 
     def get_music(self):
         if not "music" in self.sources:
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,11 +1,22 @@
 import unittest
 from rkive.clients.config import Config as Config
+import  tempfile
 
 class TestGetConfig(unittest.TestCase):
 
+    def test_no_sources(self):
+        t =  tempfile.gettempdir()
+        c = Config(t)
+        self.assertFalse(c.read_sources())
+
+    def test_no_connections(self):
+        t =  tempfile.gettempdir()
+        c = Config(t)
+        self.assertFalse(c.read_connections())
+
     def test_sources(self):
         c = Config('data/config/')
-        c.read()
+        self.assertTrue(c.read_sources())
 
         m = c.get_music()
         self.assertIsNone(m)
@@ -17,9 +28,8 @@ class TestGetConfig(unittest.TestCase):
 
     def test_connections(self):
         c = Config('data/config')
-        c.read()
+        c.read_connections()
         conns = c.get_live_connections()
-        self.assertIsNone(conns)
         self.assertEquals(1,len(conns))
         url = ''
         self.assertIn(url, conns)
