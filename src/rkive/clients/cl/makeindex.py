ea372fb328d796438d468ca5f70281ad9597db97
roger.day <c.roger.day@gmail.com>
2016-10-03 22:10:03 +0100

    add comments, refactor makeindex, remove redundant module

--- src/rkive/clients/cl/makeindex.py
+++ src/rkive/clients/cl/makeindex.py
@@ -6,29 +6,46 @@ from logging import getLogger
 import sys
 from rkive.index.musicfile import MusicFile
 from rkive.index.schema import Base, Movie
+from sqlalchemy import create_engine, sessionmaker
 
 class MakeIndexClient(GetOpts):
 
-    def __init__(self, logloc=None, urls=[], sources=[]):
+    def __init__(self, logloc='.', urls=[], sources=[]):
+        """ Initialise class - logs, command-line parameters, urls and sources
+
+        Attributes:
+            logloc: filepath location of log
+            urls: A list of urls, each url describing a database target
+            sources: A dictionary of categories to folders, each folder containing music or movies
+        """
+        p = self.get_parser()
+        p.parse_args(namespace=self)
+        LogInit().set_logging(location=logloc,filename='index.log', debug=self.debug, console=self.console)
+        self.urls = urls
+        self.sources = sources
+
+    def run(self):
+        """search the source folders that have been passed, into the databases
+        currently only movies and music are supported
+        """
+        log = getLogger('Rkive.Index')
         try:
-            p = self.get_parser()
-            p.parse_args(namespace=self)
-            LogInit().set_logging(location=logloc,filename='index.log', debug=self.debug, console=self.console)
-            for url in urls:
+            for url in self.urls:
                 engine = create_engine(url)
                 session = sessionmaker(bind=engine)
                 Base.metadata.create_all(engine)
                 Base.metadata.bind = engine
                 for category, source in sources.items():
                     if category == 'music':
-                        visit_files(base=source, funcs=[self.add_track_to_index], include=['.mp3','.flac'])
+                        visit_files(base=source, funcs=[self.add_music_to_index], include=['.mp3','.flac'])
                     if category == 'movies':
                         self.movies = set()
                         visit_files(base=source, funcs=[self.add_movie_to_index])
                         movies_in_db = Movie.get_movies_index()
                         movies_in_db_rem = movies_in_db - self.movies()
                         if movies_in_db_rem:
-                            Movie.remove_movies(self.movies)
+                            log.debug("Remove remainder databases {0}".format(movies_in_db_rem))
+                            Movie.remove_movies(self.movies_in_db_rem)
                         for m in self.movies:
                             m.save()
             return
@@ -41,8 +58,8 @@ class MakeIndexClient(GetOpts):
         finally:
             self.session.close()
 
-    def add_track_to_index(self, fp):
-        log = getLogger('Rkive')
+    def add_music_to_index(self, fp):
+        log = getLogger('Rkive.Index')
         if self.dryrun:
             log.info('would index {0}'.format(fp))
             return
@@ -50,10 +67,13 @@ class MakeIndexClient(GetOpts):
         rkive.index.mediaindex.DBSession.add(m)
 
     def add_movie_to_index(self, fp):
+        log = getLogger('Rkive.Index')
         path, leaf=os.path.split(fp)
         dp, idx = os.path.split(path)
         if idx in self.movies:
             return
         if Movie.is_movie(idx):
+            if self.dryrun:
+                log.info("Would index {0}".format(fp))
+                return
             self.movies.add(idx)
-
