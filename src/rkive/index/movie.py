345652682aa3bbbd44524375316abdd77030599e
roger.day <c.roger.day@gmail.com>
2016-11-05 20:38:56 +0000

    move out movies to own file

--- src/rkive/index/movie.py
+++ src/rkive/index/movie.py
@@ -1,55 +1,53 @@
 import re
-from rkive.index.media import Media as Media
+class Movies(object):
 
-class Movie(object):
+    film_re = re.compile('(.+?) \((.*?), (\d\d\d\d)\)')
 
-    # folder format is '<title> (<director_1, director_2 ... director_x>, <year>)'
-    film_re = re.compile('(.*?) \((.*?), (\d\d\d\d)\)')
-    session = None
+    def __init__(self, session):
+        self.session = session
 
-    def __init__(self, title, director, year):
-        self.title = title
-        self.director = []
-        for d in director:
-            self.director.append(d)
-        self.year = year
-        self.category = 'movie'
-        self.role = 'director'
+    def parse_midx(self, midx):
+        m = self.film_re.match(midx)
+        return(m.group(1),m.group(2),m.group(3))
 
-    def p(self):
-        directors = ', '.join(self.directors)
-        p='{0} ({1}, {2})'.format(self.title, directors, self.year)
-        print(p)
+    def add_movie(self, title, directors, year, fp):
+        log = getLogger('Rkive.Index')
+        log.info("title: {0}".format(title))
+        people=[]
+        directors = directors.split(', ')
+        for d in directors:
+            p = Person(d)
+            self.session.add(p)
+            mp = Moviepeople('director', p)
+            self.session.add(mp)
+            self.session.commit()
+            people.append(mp)
+        t ='mkv'
+        media = Media(t, fp)
+        self.session.add(media)
+        self.session.commit()
+        movie = Movie(title, year)
+        movie.add_person(people)
+        movie.add_media(media)
+        self.session.add(movie)
+        self.session.commit()
 
-    def save(self):
-        m = Media('mkv',self.year,self.year,self.year)
-        o = Opus(self.title, 'movie')
-        for d in self.director:
-            p = Participant(d, 'director')
-            p.opii.append(o)
-            o.participants.append(p)
-        o.mediaobjects.append(m)
-        Movie.session.add(p)
-        Movie.session.add(o)
-        Movie.session.add(m)
-        Movie.session.commit()
-
-    def get_movies():
-        return Movie.session.query(Opus.title).filter_by(category='movie').all()
-
+    def get_movies(self):
+        return self.session.query(Movie.title).all()
     #
     # returns a list of <movie_name> (<director(s)>, <year>)
-    def get_movies_index():
+    def get_movies_index(self):
+        log = getLogger('Rkive.Index')
         movie_info = set()
-        movies = Movie.session.query(Opus).filter_by(category='movie').all()
+        movies = self.session.query(Movie).all()
         for movie in movies:
-            info = Movie.get_movie_index(movie)
+            info = rkive.index.schema.Movie.get_movie_index(movie)
             movie_info.add(info)
         return movie_info
 
     def get_movie_index(movie):
         directors = []
-        for p in movie.participants:
+        for p in movie.people:
             if p.role=='director':
                 directors.append(p.name)
         year =''
@@ -59,41 +57,34 @@ class Movie(object):
         directors = ', '.join(directors)
         return '{0} ({1}, {2})'.format(movie.title, directors, year)
 
-    def find_movie(title, fmt, year,directors=[] ):
-        category = 'movie'
+    def find_movie(self, title, fmt, year,directors=[] ):
         role='director'
         movie = None
         try:
-            movie=Movie.session.query(Opus, Media).\
-            filter_by(category=category,title=title).\
-            filter(Media.media_format==fmt,Media.year_produced==year,Opus.id==Media.id).\
-            filter(and_(Opus.participants.any(Participant.role==role), Participant.name.in_(directors))).\
+            movie=self.session.query(Movie, Media).\
+            filter_by(title=title).\
+            filter(Media.format==fmt,Movie.year==year,Movie.id==Media.id).\
+            filter(and_(Movie.people.any(People.role==role), People.name.in_(directors))).\
             one()
         except alchemy_exceptions.SQLAlchemyError:
             return movie
         return movie
 
-    def is_movie(s):
-        m = Movie.film_re.match(s)
+    @classmethod
+    def is_movie(cls, root, name):
+        log = getLogger('Rkive.Index')
+        m = cls.film_re.match(name)
+        log.debug("Found: {0} {1}".format(m, name))
         if not m:
             return False
-        movie = m.group(1)
-        director = m.group(2)
-        year = m.group(3)
-        directors = director.split(', ')
-        if Movie.find_movie(movie, 'mkv', year, directors):
-            return True
-        m = Movie(movie, directors, year)
-        m.save()
         return True
 
     def remove_movies(movies):
         for movie in movies:
-            m = Movie.film_re.match(movie)
+            m = Movies.film_re.match(movie)
             title = m.group(1)
             directors = m.group(2).split(', ')
             year = m.group(3)
             movie_in_db = Movie.find_movie(title, 'mkv', year, directors)
             movie_in_db.delete(synchronize_session=True)
-
 
